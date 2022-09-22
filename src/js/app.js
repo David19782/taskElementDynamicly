d539adc2fc8960fdec41391e7a404215ac9e6986
David19782 <yordanovdavid@gmail.com>
2022-09-22 13:08:14 +0300

    feat correct app.js

--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-for(let i = 0 ; i < 5; i++){
-  const curr = document.createElement("article");
-  curr.setAttribute("class",  "message");
-  curr.innerHTML = "some content"
-  document.getElementsByTagName("body")[0].appendChild(curr);
-}
